49258f6f19439bc2d4d2a953cc925f872761c1c6
PatriciAlves <patricia.18_08@hotmail.com>
2022-06-16 17:13:25 -0300

    Service

--- src/main/java/com/generation/blogpessoal/service/PostagemService.java
+++ src/main/java/com/generation/blogpessoal/service/PostagemService.java
@@ -10,24 +10,24 @@ import org.springframework.web.server.ResponseStatusException;
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
 
-
 @Service
 public class PostagemService {
 
 	@Autowired
 	private PostagemRepository postagemRepository;
 
-
 	public Optional<Postagem> atualizaPostagem(Postagem postagem) {
+
 		if (postagemRepository.findById(postagem.getId()).isPresent()) {
 			Optional<Postagem> buscaPostagem = postagemRepository.findById(postagem.getId());
 
-			if ((buscaPostagem.isPresent()) && (buscaPostagem.get().getTema().getId() != postagem.getId()))
+			if ((buscaPostagem.isPresent()) && (buscaPostagem.get().getId() != postagem.getId()))
+
 				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema já existente no nosso sistema", null);
 
 			return Optional.ofNullable(postagemRepository.save(postagem));
 		}
 		return Optional.empty();
-	}
 
+	}
 }
